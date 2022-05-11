d50aa10a5910c0b737af3bf1f03b2adeb1710a92
Robson Nogueira <robsonngr@gmail.com>
2022-05-11 01:01:58 -0300

    Criados backend.tf e security-group.tf

---
+++ infra_basica/backend.tf
@@ -0,0 +1,13 @@
+#terraform {
+#  backend "local" {
+#      path = "terraform.tfstate"
+#  }
+#}
+
+terraform {
+  backend "s3" {
+    region = "us-east-1" # TODO substitua o valor da variavel, pela região onde você criou o bucket
+    bucket = "teste-infra-gitlab"       # TODO substitua pelo nome que você deu ao bucket
+    key    = "tfstate"
+  }
+}
